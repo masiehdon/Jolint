51431230c33a7fc1bf98c49ffa25d4a76ca54598
alisiia2601 <alisiia.ryzhykova@icloud.com>
2023-10-23 22:38:13 +0200

    navbar styling

--- src/Pages/AboutUs/index.tsx
+++ src/Pages/AboutUs/index.tsx
@@ -1,7 +1,106 @@
+import { FunctionComponent } from "react";
+import styles from "./AboutUs.module.css";
 
-
-export default function AboutUs() {
+const AboutUsFinal: FunctionComponent = () => {
   return (
-    <div>About us</div>
-  )
-}
+    <div className={styles.aboutUsFinal}>
+      <div className={styles.aboutUsFinalChild} />
+      {/* <div className={styles.groupParent}>
+  <img className={styles.instanceChild} alt="" src="/group-271.svg" /> 
+  <button className={styles.howItWorks}>
+    <div className={styles.howItWorks1}>How it works</div>
+  </button>
+  <div className={styles.inclusion}>
+    <button className={styles.howItWorks1}>Inclusion</button>
+  </div>
+  <div className={styles.faq}>
+    <button className={styles.howItWorks1}>FAQ</button>
+  </div>
+  <div className={styles.aboutUs}>
+    <button className={styles.howItWorks1}>About us</button>
+  </div>
+  <div className={styles.globeParent}>
+    <img className={styles.globeIcon} alt="" src="/globe1.svg" />
+    <img className={styles.arrowDownIcon} alt="" src="/arrowdown2.svg" />
+    <button className={styles.english}>English</button>
+  </div> 
+  <div className={styles.contactUs}>
+    <button className={styles.howItWorks1}>Contact us</button>
+  </div>
+  <div className={styles.signInKnapp}>
+    <button className={styles.signInKnapp1}>
+      <div className={styles.signInKnappChild} />
+      <b className={styles.signIn}>Consent form</b>
+    </button>
+  </div>
+</div> */}
+
+      <b className={styles.aboutUs2}>About us</b>
+      <div className={styles.atJolintWeContainer}>
+        <p className={styles.atJolintWeAreDrivenByA}>
+          <span className={styles.atJolintWe}>
+            At Jolint, we are driven by a passion for
+          </span>
+          <b className={styles.atJolintWe}>{` improving workplaces `}</b>
+          <span>{`and creating a better world for everyone. `}</span>
+        </p>
+        <p
+          className={styles.atJolintWeAreDrivenByA}
+        >{`We make sure to keep ethics and privacy at the forefront when gathering data. Throughout our journey, `}</p>
+        <p className={styles.atJolintWeAreDrivenByA}>
+          <span>we've stayed committed to these principles, ensuring that</span>
+          <b
+            className={styles.atJolintWe}
+          >{` individuals' information is handled with care and respect.  `}</b>
+          <span>{`Discover the journey of Jolint, and join us in our quest to make the world a better place, one company at a time. `}</span>
+        </p>
+        <p
+          className={styles.atJolintWeAreDrivenByA}
+        >{`With our innovative approach, cutting-edge technologies, and a team of experts, we are committed to creating `}</p>
+        <p className={styles.atJolintWeAreDrivenByA}>
+          <span>a</span>
+          <b className={styles.atJolintWe}>
+            {" "}
+            more inclusive and equitable work culture for all.
+          </b>
+        </p>
+        <p className={styles.atJolintWeAreDrivenByA}>&nbsp;</p>
+      </div>
+      <b className={styles.ourMission}>Our mission</b>
+      <b className={styles.coFounders}>Co-founders:</b>
+      <div className={styles.gradiant2} />
+      <img className={styles.image17Icon} alt="" src="/image-17@2x.png" />
+      <div className={styles.gradiant21}>
+        <img className={styles.image16Icon} alt="" src="/image-16@2x.png" />
+        <div className={styles.iAmA}>
+          I am a data scientist and physicist. My background in Complex Adaptive
+          Systems has fueled my passion for understanding how people interact,
+          communicate, and collaborate in the workplace. My mission is to use
+          data-driven analysis to make the workplace a better place for
+          employees, by understanding the complexities of human interactions and
+          finding ways to improve them. Join me on my journey to enhance
+          workplace culture and improve the work environment. With my expertise
+          in data analysis and understanding of human behavior, I am committed
+          to leading the way towards a more inclusive and equitable work culture
+          for all.
+        </div>
+        <div className={styles.asTheCoFounder}>
+          As the co-founder of Jolint, I bring a wealth of expertise in
+          organizational communication and collaboration to the table. With a
+          background in international politics, communication studies, and
+          specialized training in these areas, I understand the intricacies and
+          nuances of effective communication within organizations. My passion
+          for this field led me to start Jolint, with the goal of empowering
+          companies to create more inclusive and equitable work cultures. Join
+          me in my journey with Jolint, as we work together to improve
+          communication and collaboration within organizations for a more
+          inclusive work environment.
+        </div>
+      </div>
+      <div className={styles.klaudiaMur}>Klaudia Mur</div>
+      <div className={styles.faheemShah}>Faheem Shah</div>
+    </div>
+  );
+};
+
+export default AboutUsFinal;
--- src/Pages/Navbar/index.tsx
+++ src/Pages/Navbar/index.tsx
@@ -1,132 +1,42 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import { Link } from 'react-router-dom';
-// import SplitButton from '../../components/SplitButton';
-import logo from '../../assets/logo.svg'
-import { ButtonGroup } from '@mui/material';
+import React from 'react';
+import { FunctionComponent } from 'react';
+import styles from './navbar.module.css';
 
-const pages = ['How it works', 'Why Inclusion', 'FAQ', 'About us', 'Contact us'];
-// const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
- 
-
-const btnSignIn = '#f4770a'
-
-function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  // const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-  
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-//  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-//    setAnchorElUser(event.currentTarget);
-//  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  // const handleCloseUserMenu = () => {
-  //   setAnchorElUser(null);
-  // };
-
+const ResponsiveAppBar: FunctionComponent = () => {
   return (
-    
-    <AppBar position="static" sx={{backgroundColor: 'white',}}>
-      <Container maxWidth="xl">
-        <Toolbar sx={{ justifyContent: 'space-between'}} disableGutters  >
-     
-          <Avatar alt='Jolint' src={logo} sx={{ minWidth: 150, minHeight: 0, marginLeft: 0}}/>
-         
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none',  } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="default"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography
-                    textAlign="center">
-                    
-                  <Link to={`/${page.replace(/\s+/g, '-')}`}>
-                    {page}
-                    </Link>
-
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                <Link to={`/${page.replace(/\s+/g, '-')}`}>
-                {page}
-                </Link>
-              </Button>
-            ))}
-          </Box>
-          <ButtonGroup
-  disableElevation
-  variant="text"
-  aria-label="Disabled elevation buttons"
->
-          {/* <SplitButton /> */}
-         <Button
-        style={{ backgroundColor: btnSignIn, color: 'white', borderRadius: 10}}
-        variant="contained"
-        onClick={() => {
-          // Handle button click
-        }}
-            >
-              consent form
-            </Button>
-          </ButtonGroup>
-
-        </Toolbar>
-      </Container>
-      </AppBar>
-    
+    <div className={styles.aboutUsFinal}>
+      <div className={styles.aboutUsFinalChild} />
+      <div className={styles.groupParent}>
+        <img className={styles.instanceChild} alt="" src="/group-271.svg" />
+        <button className={styles.howItWorks}>
+          <div className={styles.howItWorks1}>How it works</div>
+        </button>
+        <div className={styles.inclusion}>
+          <button className={styles.howItWorks1}>Inclusion</button>
+        </div>
+        <div className={styles.faq}>
+          <button className={styles.howItWorks1}>FAQ</button>
+        </div>
+        <div className={styles.aboutUs}>
+          <button className={styles.howItWorks1}>About us</button>
+        </div>
+        <div className={styles.globeParent}>
+          <img className={styles.globeIcon} alt="" src="/globe1.svg" />
+          <img className={styles.arrowDownIcon} alt="" src="/arrowdown2.svg" />
+          <button className={styles.english}>English</button>
+        </div>
+        <div className={styles.contactUs}>
+          <button className={styles.howItWorks1}>Contact us</button>
+        </div>
+        <div className={styles.signInKnapp}>
+          <button className={styles.signInKnapp1}>
+            <div className={styles.signInKnappChild} />
+            <b className={styles.signIn}>Consent form</b>
+          </button>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
 export default ResponsiveAppBar;
