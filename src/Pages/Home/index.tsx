c49269bf86ef18199fe58ef177ec50dd5fa8deb9
alisiia2601 <alisiia.ryzhykova@icloud.com>
2023-10-24 01:05:12 +0200

    landing page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,78 +1,206 @@
-import { Avatar, Button, Card, Paper, Stack, Typography } from '@mui/material';
-import Grid from '@mui/material/Grid';
-import { content } from '../../mockdata/home';
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import { inclusion, InclusionDifference } from '../..//mockdata/inclusion';
-import Advantages from '../Advantages';
+import { FunctionComponent } from "react";
+import styles from "./landing.module.css";
 
-
-
-const btnColor = '#F4770A'
-const percentageColor= '#0015CE'
-  
-  
-export default function index() {
-
-  
-
+const StartsidaFinalVersion: FunctionComponent = () => {
   return (
-    
-    <Grid container>
-     
-        {content.map((item, index) => (
-         <Grid item xs={12} md={6} lg={6} >         
-          <Card key={index} sx={{ border: 'none', boxShadow: 'none' }}>
-          <Stack sx={{m:3}}>
-          <Typography variant='h5'>
-              {item.header}
-            </Typography> 
-            <Typography variant='subtitle1'>
-              {item.text}
-              </Typography>
-              <Grid item xs={6} sm={12}>
-              <Button variant='outlined' sx={{m:1}}>Why inclusion </Button>
-               <Button variant='contained' sx={{m:1}}><ArrowRightIcon />How it works</Button>
-              </Grid>
-              <Grid item xs={6} sm={12}>
-                  <Button variant='contained' sx={{ backgroundColor: btnColor, m: 1 }}>Consent form</Button>
-              
-                </Grid>
-              </Stack>
-            </Card>
-          </Grid>
-        ))}
-      <Grid item xs={12} md={6} lg={6}>
-        <Card  sx={{
-          border: 'none', boxShadow: 'none'
-        }}>
-          {
-            <Avatar
-            src="https://cdn.pixabay.com/photo/2019/10/06/10/03/team-4529717_1280.jpg"
-            alt="Description"
-              sx={{
-                height: .8, width: .8, overflow: 'hidden', position: 'relative', ml: 25, mt: 5
-                 }}
-          />}
-        </Card>
-      </Grid>
-      
-      <Grid item xs={12} sx={{mt: 10, ml:5, mb: 5, textAlign: 'center'}} >
-        <Card sx={{ border: 'none', boxShadow: 'none',  }} >
-        <Typography variant='h4'>Inclusion makes a difference</Typography>
-          <Typography variant='body2'>Organisations with higher inclusion among coworkers have according to [source]:
-          </Typography>
-        </Card>
-      </Grid>
-      
-      {inclusion.map((item: InclusionDifference, index: number) => (
-        <Grid item xs={6} md={3} sx={{textAlign: 'center'}}>
-          <Paper elevation={0} key={index}>
-          <Typography variant="body2" sx={{color: percentageColor, fontWeight: 700}}>{item.percentage}%</Typography>
-      <Typography variant="body2">{item.text}</Typography>
-          </Paper>
-           </Grid>
-        ))}
-        <Advantages />
-      </Grid>
-  )
-}
+    <div className={styles.startsidaFinalVersion}>
+      <div className={styles.rutaFrBilderParent}>
+        <div className={styles.rutaFrBilder}>
+          <div className={styles.rutaFrBilderChild} />
+        </div>
+        <div className={styles.groupChild} />
+      </div>
+      <b className={styles.letsCreateAn}>
+        Let’s Create an Inclusive Workspace Together
+      </b>
+      <div className={styles.startsidaFinalVersionChild} />
+      <div className={styles.higherTurnover}>Higher turnover</div>
+      <div className={styles.lowerSickLeave}>Lower sick leave</div>
+      <div className={styles.moreLoyalCustomers}>More loyal customers</div>
+      <div className={styles.lowerStaffTurnover}>Lower staff turnover</div>
+      <div className={styles.div}>21%</div>
+      <div className={styles.div1}>41%</div>
+      <div className={styles.div2}>59%</div>
+      <div className={styles.div3}>200%</div>
+      <b className={styles.inclusionMakesA}>Inclusion makes a difference</b>
+      <div
+        className={styles.organisationsWithHigher}
+      >{`Organisations with higher inclusion among coworkers have: `}</div>
+      <div className={styles.startsidaFinalVersionChild} />
+      <div className={styles.lessIsolation}>Less isolation</div>
+      <div className={styles.lowerSickLeave1}>Lower sick leave</div>
+      <div className={styles.moreSatisfiedEmployees}>
+        More satisfied employees
+      </div>
+      <div className={styles.lowerStaffTurnover1}>Lower staff turnover</div>
+      <div className={styles.div4}>31%</div>
+      <div className={styles.div5}>41%</div>
+      <div className={styles.div6}>59%</div>
+      <div className={styles.div7}>61%</div>
+      <b className={styles.inclusionMakesA}>Inclusion makes a difference</b>
+      <div
+        className={styles.organisationsWithHigher1}
+      >{`Organisations with higher inclusion among coworkers have according to [source]: `}</div>
+      <img
+        className={styles.startsidaFinalVersionInner}
+        alt=""
+        src="/group-26.svg"
+      />
+      <img className={styles.ellipseIcon} alt="" src="/ellipse-34@2x.png" />
+      <b className={styles.advantagesWithJolint}>
+        Advantages with Jolint at your workplace
+      </b>
+      <div className={styles.anonymousAndObjectiveContainer}>
+        <p className={styles.anonymousAnd}>Anonymous and</p>
+        <p className={styles.anonymousAnd}>objective analysis</p>
+      </div>
+      <img className={styles.polygonIcon} alt="" src="/polygon-3.svg" />
+      <div className={styles.howItWorksKnapp}>
+        <div className={styles.howItWorksKnappChild} />
+        <div className={styles.howItWorksParent}>
+          <b className={styles.howItWorks}>How it works</b>
+          <img className={styles.groupItem} alt="" src="/polygon-3.svg" />
+        </div>
+      </div>
+      <div className={styles.whyInclusionKnapp}>
+        <div className={styles.whyInclusionKnappChild} />
+        <div className={styles.whyInclusion}>Why inclusion</div>
+      </div>
+      <div className={styles.gainValuableData}>
+        Gain valuable data insights with our solution that measures inclusion
+        and belonging within your organization, enabling you to take action for
+        a more inclusive and vibrant work culture while maintaining the privacy
+        and integrity of your personal data
+      </div>
+      <div className={styles.signInKnapp}>
+        <div className={styles.signInKnappChild} />
+        <b className={styles.consentForm}>Consent form</b>
+      </div>
+      <div className={styles.hover}>
+        <div className={styles.anonymousAndObjective}>
+          Anonymous and objective analysis
+        </div>
+      </div>
+      <div className={styles.hover1}>
+        <div className={styles.hoverChild} />
+        <div className={styles.moreAwareEmployers}>More aware employers</div>
+      </div>
+      <div className={styles.hover2}>
+        <div className={styles.anonymousAndObjective}>
+          Healthier work environment
+        </div>
+      </div>
+      <div className={styles.howItWorksFinalVersionTe}>
+        <div className={styles.howItWorksFinalVersionTeChild} />
+        <div className={styles.howItWorksFinalVersionTeItem} />
+        <img className={styles.streckIcon} alt="" src="/streck1.svg" />
+        <img className={styles.streckIcon1} alt="" src="/streck2.svg" />
+        <div className={styles.howItWorksFinalVersionTeInner} />
+        <div className={styles.ellipseDiv} />
+        <div className={styles.howItWorksFinalVersionTeChild1} />
+        <div className={styles.howItWorksFinalVersionTeChild2} />
+        <img className={styles.streckIcon2} alt="" src="/streck2.svg" />
+        <div className={styles.howItWorksFinalVersionTeChild3} />
+        <img className={styles.streckIcon3} alt="" src="/streck3.svg" />
+        <div className={styles.howItWorksFinalVersionTeChild4} />
+        <div className={styles.jolintPullsOut}>
+          Jolint pulls out interaction info from common work tools like email,
+          but without touching any personal details. Imagine it like seeing
+          where conversations happen without knowing who said what. This way,
+          Jolint gets a bird's-eye view of how people connect at work while
+          keeping everyone's privacy intact.
+        </div>
+        <div className={styles.dataExtraction}>{`Data Extraction `}</div>
+        <div className={styles.jolintGivesEveryone}>
+          Jolint gives everyone an anonymized ID, replacing real names and
+          emails to keep things private. So, even when looking at how people
+          interact, there's no way to tell who's who, ensuring everyone's
+          privacy while still understanding the big picture of connections at
+          work.
+        </div>
+        <div className={styles.anonymization}>Anonymization</div>
+        <div className={styles.insightsFromThe}>
+          Insights from the data analysis are showcased in an intuitive
+          interface, offering aggregated-level insights for easier understanding
+          and action. It highlights key drivers of inclusion and recommends
+          activities tailored to your company's specific challenges, aiding in
+          informed decision-making.
+        </div>
+        <div className={styles.insightsInterface}>Insights Interface</div>
+        <div className={styles.dataIsTransformed}>
+          Data is transformed into a network representation where each employee
+          is a node and interactions form connections. Each person becomes a
+          point on the web, and the lines between them show conversations. This
+          setup helps Jolint see how well everyone is connected, without knowing
+          any personal details.
+        </div>
+        <div className={styles.dataProcessing}>Data Processing</div>
+        <div className={styles.aiAndMachine}>
+          AI and machine learning analyze data to pinpoint drivers of inclusion
+          and belonging. They seek patterns to address questions such as new
+          hire integration speed or how to elevate inclusion scores by
+          addressing organizational weak points. The analysis also delves into
+          social capital variations based on gender, role, and hire timing..
+        </div>
+        <div className={styles.aiAnalysis}>AI Analysis</div>
+        <img className={styles.groupIcon} alt="" src="/group-2127.svg" />
+        <img className={styles.groupIcon} alt="" src="/group-2127.svg" />
+        <img
+          className={styles.howItWorksFinalVersionTeChild6}
+          alt=""
+          src="/group-2128.svg"
+        />
+        <img
+          className={styles.howItWorksFinalVersionTeChild7}
+          alt=""
+          src="/group-2129.svg"
+        />
+        <img
+          className={styles.howItWorksFinalVersionTeChild8}
+          alt=""
+          src="/group-2130.svg"
+        />
+        <img
+          className={styles.howItWorksFinalVersionTeChild9}
+          alt=""
+          src="/group-2131.svg"
+        />
+        <div className={styles.howItWorksGroup}>
+          <b className={styles.howItWorks1}>How it works</b>
+          <img className={styles.volume2Icon} alt="" src="/volume2.svg" />
+        </div>
+      </div>
+      <div className={styles.groupParent}>
+        <img className={styles.frameChild} alt="" src="/group-27.svg" />
+        <div className={styles.howItWorks2}>
+          <div className={styles.howItWorks3}>How it works</div>
+        </div>
+        <div className={styles.inclusion}>
+          <div className={styles.howItWorks3}>Why Inclusion</div>
+        </div>
+        <div className={styles.faq}>
+          <div className={styles.howItWorks3}>FAQ</div>
+        </div>
+        <div className={styles.aboutUs}>
+          <div className={styles.howItWorks3}>About us</div>
+        </div>
+        <div className={styles.globeParent}>
+          <img className={styles.globeIcon} alt="" src="/globe.svg" />
+          <img className={styles.arrowDownIcon} alt="" src="/arrowdown.svg" />
+          <div className={styles.english}>English</div>
+        </div>
+        <div className={styles.contactUs}>
+          <div className={styles.howItWorks3}>Contact us</div>
+        </div>
+        <div className={styles.signInKnapp1}>
+          <div className={styles.signInKnapp2}>
+            <div className={styles.signInKnappChild} />
+            <b className={styles.consentForm}>Consent form</b>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StartsidaFinalVersion;
