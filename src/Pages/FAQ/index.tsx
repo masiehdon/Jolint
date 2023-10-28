bb3cb09fec10758779235d24e6f93885a82f4628
Moe <masihdon@hotmail.de>
2023-10-28 21:30:16 +0200

    'modified faq'

--- src/Pages/FAQ/index.tsx
+++ src/Pages/FAQ/index.tsx
@@ -1,91 +1,31 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
-import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
-import MuiAccordionSummary, {
-  AccordionSummaryProps,
-} from '@mui/material/AccordionSummary';
-import MuiAccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
 import faqs from '../../mockdata/faqs.js';
-import { Box } from '@mui/material';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-const Accordion = styled((props: AccordionProps) => (
-  <MuiAccordion disableGutters elevation={0} square {...props} />
-))(({ theme }) => ({
-  border: `1px solid ${theme.palette.divider}`,
-  '&:not(:last-child)': {
-    borderBottom: 0,
-  },
-  '&:before': {
-    display: 'none',
-  },
-}));
-
-const AccordionSummary = styled((props: AccordionSummaryProps) => (
-  <MuiAccordionSummary
-    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
-    {...props}
-  />
-))(({ theme }) => ({
-  backgroundColor:
-    theme.palette.mode === 'dark'
-      ? 'rgba(255, 255, 255, .05)'
-      : 'rgba(0, 0, 0, .03)',
-  flexDirection: 'row-reverse',
-  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-    transform: 'rotate(90deg)',
-  },
-  '& .MuiAccordionSummary-content': {
-    marginLeft: theme.spacing(1),
-  },
-}));
-
-const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-  padding: theme.spacing(2),
-  borderTop: '1px solid rgba(0, 0, 0, .125)',
-}));
-
-export default function CustomizedAccordions() {
-  const [, setExpanded] = React.useState<string | false>('panel1');
-
-  const handleChange =
-    (panel: string) => (_event: React.SyntheticEvent, newExpanded: boolean) => {
-      setExpanded(newExpanded ? panel : false);
-    };
-
+export default function Faqs() {
   return (
     <div>
-      <Box>
-      <Typography 
-        variant='h4'
-        sx={{ m: 5 }} 
-              >
-                FAQs
-        </Typography>
-      </Box>
-      {
-            faqs.map((faq, id) => (
-              
-              <Accordion
-                key={id}
-                onChange={handleChange('panel1')}
-                sx={{
-        border: '1px solid black'
-    }}
-              >
-        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-        
-                  <Typography variant='h5'>{ faq.Q }</Typography>
+      {faqs.map((faq, index) => (
+       <Accordion key={index} sx={{ml: 5, mr: 5, mt: 10}}>
+          <AccordionSummary
+            sx={{ml: 5, mr: 5, mt: 10}}
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography variant='h5'>{ faq.Q}</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography variant='h5'>
-            {faq.A}
+        <AccordionDetails sx={{ml: 5, mr: 5, background: '#E5E4E2'}}>
+          <Typography variant='h6'>
+           {faq.A}
           </Typography>
         </AccordionDetails>
       </Accordion>
-        ))}
-
+     ))}
+      
     </div>
   );
 }
