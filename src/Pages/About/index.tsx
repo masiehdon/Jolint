4084fe36cccb6117ff4244ead9ed93b53e479acf
Moe <masihdon@hotmail.de>
2023-11-13 23:20:00 +0100

    make about us page responsive

---
+++ src/Pages/About/index.tsx
@@ -0,0 +1,12 @@
+import { Grid } from "@mui/material";
+
+
+export default function index() {
+  return (
+      <div>
+          <Grid container>
+              
+          </Grid>
+    </div>
+  )
+}
