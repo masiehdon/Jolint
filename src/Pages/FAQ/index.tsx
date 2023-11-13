e3615e3f2eb4beb4e60a7e1c6793a2d28b62bedd
Moe <masihdon@hotmail.de>
2023-11-13 12:39:35 +0100

    update the last update

--- src/Pages/FAQ/index.tsx
+++ src/Pages/FAQ/index.tsx
@@ -4,12 +4,16 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Box } from '@mui/material';
 
 export default function Faqs() {
   return (
-    <div>
+    <Box sx={{minWidth: '100vw'}}>
       {faqs.map((faq, index) => (
-       <Accordion key={index} sx={{ml: 5, mr: 5, mt: 10}}>
+        <Accordion key={index} sx={{
+          ml: 5, mr: 5, maxWidth: {xs: '100%', sm: '90%', lg: '80%'}, '&.Mui-expanded': {
+              ml: 5, 
+            }, }}>
           <AccordionSummary
             sx={{ml: 5, mr: 5, mt: 10}}
           expandIcon={<ExpandMoreIcon />}
@@ -18,14 +22,14 @@ export default function Faqs() {
         >
           <Typography variant='h5'>{ faq.Q}</Typography>
         </AccordionSummary>
-        <AccordionDetails sx={{ml: 5, mr: 5, background: '#E5E4E2'}}>
-          <Typography variant='h6'>
+        <AccordionDetails sx={{ml: 5, mr: 5,  }}>
+          <Typography variant='h6' sx={{fontSize: 20}} >
            {faq.A}
           </Typography>
         </AccordionDetails>
       </Accordion>
      ))}
       
-    </div>
+    </Box>
   );
 }
